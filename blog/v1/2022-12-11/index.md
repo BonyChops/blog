09a70c58c26b633ed8b8f2d29a50b0f13c41ed35
BonyChops <contact@bonychops.com>
2022-12-11 14:12:12 +0900

    Init

---
+++ blog/v1/2022-12-11/index.md
@@ -0,0 +1,40 @@
+---
+slug: description
+title: ここは何
+authors: [bonychops]
+tags: []
+---
+
+ほねつき備忘録に掲載したブログもこちらに移行する予定です．Docusaurusはこんな感じに**マルチブログ**にも対応しています．
+
+## やり方
+アドカレからきた方が多いと思いますので，やり方を記載しておきます[^1]．
+`docusaurus.config.js`を次のように編集します．
+
+```js title="docusaurus.config.js"
+module.exports = {
+  // ...
+  plugins: [
+    [
+      '@docusaurus/plugin-content-blog',
+      {
+        /**
+         * マルチインスタンスプラグインに置いて必ず必要
+         */
+        id: 'second-blog',
+        /**
+         * ブログのURL
+         * 末尾のスラッシュ(/)は**含めない**
+         */
+        routeBasePath: 'my-second-blog',
+        /**
+         * ブログファイルが置かれているディレクトリパス
+         */
+        path: './my-second-blog',
+      },
+    ],
+  ],
+};
+```
+
+[^1]: [Multiple blogs | Docusaurus](https://docusaurus.io/docs/next/blog#multiple-blogs)
---
+++ docusaurus.config.js
@@ -0,0 +1,134 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'My Site',
+  tagline: 'Dinosaurs are cool',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'ja',
+    locales: ['ja'],
+  },
+
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: false,
+        blog: {
+          routeBasePath: "/",
+          showReadingTime: true,
+          path: './blog/current',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  plugins: [
+    [
+      '@docusaurus/plugin-content-blog',
+      {
+        /**
+         * Required for any multi-instance plugin
+         */
+        id: 'blog-v1',
+        /**
+         * URL route for the blog section of your site.
+         * *DO NOT* include a trailing slash.
+         */
+        routeBasePath: '/v1',
+        /**
+         * Path to data on filesystem relative to site dir.
+         */
+        path: './blog/v1',
+      },
+    ]
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Blogs',
+        logo: {
+          alt: 'Bony_Chops',
+          src: 'https://github.com/bonychops.png',
+        },
+        items: [
+          {
+            href: '/v1',
+            label: 'ほねつき備忘録アーカイブ',
+          },
+          {
+            href: 'https://bonychops.hatenablog.jp/',
+            label: 'ほねつき備忘録(Old Blog)',
+            position: 'right',
+          },
+          {
+            href: 'https://github.com/BonyChops/blog',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Socials',
+            items: [
+              {
+                label: 'bonychops.com',
+                href: 'https://bonychops.com',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/BonyChops',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/BonyChops',
+              },
+              {
+                label: 'Others',
+                href: 'https://bonychops.com/socials',
+              },
+
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Bony_Chops. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
