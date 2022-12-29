d0a20715d36d73e0630655bdba117c2220b62611
BonyChops <contact@bonychops.com>
2022-12-29 13:06:16 +0900

    Fix: site name

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: 'Bony_Chops Blog',
   tagline: 'Dinosaurs are cool',
   url: 'https://blog.b7s.dev',
   baseUrl: '/',
