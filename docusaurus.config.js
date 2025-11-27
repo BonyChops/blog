808dfdde63e7f22823f5709b2c163f9b202bfb03
BonyChops <contact@bonychops.com>
2025-11-27 14:28:20 +0900

    feat: set postsPerPage to all

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -52,7 +52,8 @@ const config = {
           editUrl: "https://github.com/BonyChops/blog/blob/main/",
           remarkPlugins: [math],
           rehypePlugins: [katex],
-          showLastUpdateTime: true
+          showLastUpdateTime: true,
+          postsPerPage: "ALL"
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
